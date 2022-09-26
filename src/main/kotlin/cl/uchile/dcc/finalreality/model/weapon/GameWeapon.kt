4854c619af1715dfe8660bec080e38d1f25dc62a
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-09-26 15:55:44 -0300

    GameWeapon interface documentation

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/GameWeapon.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/weapon/GameWeapon.kt
@@ -1,5 +1,19 @@
 package cl.uchile.dcc.finalreality.model.weapon
 
+/**
+ * This represents a character from the game.
+ * A character can be controlled by the player or by the CPU (an enemy).
+ *
+ * @property name
+ *    The name of the weapon.
+ * @property damage
+ *    The damage to the weapon.
+ * @property weight
+ *    The weight of this weapon.
+ *
+ * @author <a href="https://github.com/Lucas-CE">Lucase</a>
+ */
+
 interface GameWeapon {
     val name: String
     val damage: Int
