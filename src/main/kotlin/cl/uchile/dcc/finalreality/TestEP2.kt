e2801af5bcdc4959807d89e783d896a53dbecc4e
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-09-26 22:19:49 -0300

    Creation example TestEP2

--- src/main/kotlin/cl/uchile/dcc/finalreality/TestEP2.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/TestEP2.kt
@@ -2,21 +2,31 @@ package cl.uchile.dcc.finalreality
 
 import cl.uchile.dcc.finalreality.model.character.Enemy
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
-import cl.uchile.dcc.finalreality.model.character.player.Thief
-import cl.uchile.dcc.finalreality.model.weapon.types.Knife
+import cl.uchile.dcc.finalreality.model.character.player.Knight
+import cl.uchile.dcc.finalreality.model.character.player.WhiteMage
+import cl.uchile.dcc.finalreality.model.weapon.types.Staff
+import cl.uchile.dcc.finalreality.model.weapon.types.Sword
 import java.util.concurrent.LinkedBlockingQueue
 import kotlin.random.Random
 
 fun main() {
+
     val queue = LinkedBlockingQueue<GameCharacter>()
-    for (i in 0 until 10) {
-        // Gives a random speed to each character to generate different waiting times
-        val weapon = Knife("", 0, Random.nextInt(1, 50))
-        val character = Thief("$i", 10, 10, queue)
-        character.equip(weapon)
-        character.waitTurn()
-    }
-    val enemy = Enemy("ene", 80, 100, 10, queue)
+
+    val hero1 = Knight("Hero 1", 10, 10, queue)
+    val rapier = Sword("Rapier", 9, 5)
+    hero1.equip(rapier)
+
+    val hero2 = WhiteMage("Heroe 2", 10, 5, 5, queue)
+    val crosier = Staff("crosier", 14, Random.nextInt(1, 50))
+    hero2.equip(crosier)
+
+    hero1.waitTurn()
+    hero2.waitTurn()
+
+    val evilElf = Enemy("Villain", 10, 100, 10, queue)
+    evilElf.waitTurn()
+
     // Waits for 6 seconds to ensure that all characters have finished waiting
     Thread.sleep(6000)
     while (!queue.isEmpty()) {
