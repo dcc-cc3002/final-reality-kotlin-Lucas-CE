7c8ba1970e61ee5c08df12d93c3ab45287534cba
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-10-23 07:57:05 -0300

    Test Engineer creation

---
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/EngineerSpec.kt
@@ -0,0 +1,56 @@
+package cl.uchile.dcc.finalreality.model.character.player
+
+import cl.uchile.dcc.finalreality.model.character.GameCharacter
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.positiveInt
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import java.util.concurrent.LinkedBlockingQueue
+
+private const val ENG1_NAME = "ENG1"
+private const val ENG1_MAXHP = 100
+private const val ENG1_DEFENSE = 10
+private const val ENG2_NAME = "ENG2"
+private const val ENG2_MAXHP = 80
+private const val ENG2_DEFENSE = 20
+
+
+class EngineerSpec : FunSpec({
+    lateinit var Eng1: Engineer
+    lateinit var Eng2: Engineer
+    val queue = LinkedBlockingQueue<GameCharacter>()
+
+    beforeEach {
+        Eng1 = Engineer(ENG1_NAME, ENG1_MAXHP, ENG1_DEFENSE, queue)
+        Eng2 = Engineer(ENG2_NAME, ENG2_MAXHP, ENG2_DEFENSE, queue)
+    }
+
+    test("toString must return the Engineer description") {
+        checkAll(Arb.string(), Arb.positiveInt(1000), Arb.positiveInt(100))
+        { name, maxHp, defense ->
+            val Eng3 = Engineer(name, maxHp, defense, queue)
+            Eng3.toString() shouldBe "Engineer {name='$name', maxHp='$maxHp', defense='$defense'}"
+        }
+    }
+
+    test("Two Engineers with the same parameters are equals") {
+        checkAll(Arb.string(), Arb.positiveInt(1000), Arb.positiveInt(100))
+        { name, maxHp, defense ->
+            val Eng31 = Engineer(name, maxHp, defense, queue)
+            val Eng32 = Engineer(name, maxHp, defense, queue)
+            Eng31 shouldNotBeSameInstanceAs Eng32
+            Eng31 shouldBe Eng32
+        }
+    }
+
+    test("Two Engineers with different parameters are not equals") {
+        Eng1 shouldNotBeSameInstanceAs Eng2
+        Eng1 shouldNotBe Eng2
+    }
+
+
+})
