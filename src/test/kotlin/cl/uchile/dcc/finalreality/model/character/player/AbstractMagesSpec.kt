4820328f4e6586daa7aa3471ac08468d34b91c55
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-10-23 13:43:21 -0300

    Update AbstractMage test

--- src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/AbstractMagesSpec.kt
+++ src/test/kotlin/cl/uchile/dcc/finalreality/model/character/player/AbstractMagesSpec.kt
@@ -10,14 +10,10 @@ import io.kotest.property.checkAll
 import java.util.concurrent.LinkedBlockingQueue
 import org.junit.jupiter.api.assertThrows
 
-private const val BLMG_NAME = "BLMG"
-private const val BLMG_MAXHP = 100
-private const val BLMG_MAXMP = 30
-private const val BLMG_DEFENSE = 10
-private const val WHMG_NAME = "WHMG"
-private const val WHMG_MAXHP = 80
-private const val WHMG_MAXMP = 40
-private const val WHMG_DEFENSE = 20
+private const val MAGE_NAME = "MAGE1"
+private const val MAGE_MAXHP = 100
+private const val MAGE_MAXMP = 30
+private const val MAGE_DEFENSE = 10
 
 class AbstractMagesSpec : FunSpec ({
     lateinit var BlackMage1: BlackMage
@@ -25,23 +21,23 @@ class AbstractMagesSpec : FunSpec ({
     val queue = LinkedBlockingQueue<GameCharacter>()
 
     beforeEach {
-        BlackMage1 = BlackMage(BLMG_NAME, BLMG_MAXHP, BLMG_MAXMP, BLMG_DEFENSE, queue)
-        WhiteMage1 = WhiteMage(WHMG_NAME, WHMG_MAXHP, WHMG_MAXMP, WHMG_DEFENSE, queue)
+        BlackMage1 = BlackMage(MAGE_NAME, MAGE_MAXHP, MAGE_MAXMP, MAGE_DEFENSE, queue)
+        WhiteMage1 = WhiteMage(MAGE_NAME, MAGE_MAXHP, MAGE_MAXMP, MAGE_DEFENSE, queue)
     }
 
     test("The currentMp initial value is the maxMp value"){
-        BlackMage1.currentMp shouldBe BLMG_MAXMP
-        WhiteMage1.currentMp shouldBe WHMG_MAXMP
+        BlackMage1.currentMp shouldBe MAGE_MAXMP
+        WhiteMage1.currentMp shouldBe MAGE_MAXMP
     }
 
     test("The currentMp setter change the currentMp value"){
-        checkAll(Arb.int(0..BLMG_MAXMP))
+        checkAll(Arb.int(0..MAGE_MAXMP))
         { currentMp ->
             BlackMage1.currentMp = currentMp
             BlackMage1.currentMp shouldBe currentMp
         }
 
-        checkAll(Arb.int(0..WHMG_MAXMP))
+        checkAll(Arb.int(0..MAGE_MAXMP))
         { currentMp ->
             WhiteMage1.currentMp = currentMp
             WhiteMage1.currentMp shouldBe currentMp
@@ -52,24 +48,18 @@ class AbstractMagesSpec : FunSpec ({
 
         //Test blackMage
         checkAll(
-            Arb.int(BLMG_MAXMP+1..2*BLMG_MAXMP),
-            Arb.int(-WHMG_MAXMP..-1)
+            Arb.int(MAGE_MAXMP+1..2*MAGE_MAXMP),
+            Arb.int(-MAGE_MAXMP..-1)
         )
         { greaterMaxMp, lessMinMp ->
+            //BlackMage
             assertThrows<InvalidStatValueException> {
                 BlackMage1.currentMp = greaterMaxMp
             }
             assertThrows<InvalidStatValueException> {
                 BlackMage1.currentMp = lessMinMp
             }
-        }
-
-        //Test whiteMage
-        checkAll(
-            Arb.int(WHMG_MAXMP+1..2*WHMG_MAXMP),
-            Arb.int(-WHMG_MAXMP..-1)
-        )
-        { greaterMaxMp, lessMinMp ->
+            //WhiteMage
             assertThrows<InvalidStatValueException> {
                 WhiteMage1.currentMp = greaterMaxMp
             }
