7532a2e12878f2c7a1d177caa3d0076f82101c95
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-11-03 10:13:24 -0300

    player class name variable name changed in invalid equipped weapon exception

--- src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/InvalidEquippedWeaponException.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/exceptions/InvalidEquippedWeaponException.kt
@@ -9,5 +9,5 @@ package cl.uchile.dcc.finalreality.exceptions
  * @author <a href="https://github.com/Lucas-CE">Lucase</a>
  */
 
-class InvalidEquippedWeaponException(weapon_class_name: String, class_name: String) :
-    Exception("The $weapon_class_name cannot be equipped to an $class_name")
+class InvalidEquippedWeaponException(weapon_class_name: String, player_class_name: String) :
+    Exception("The $weapon_class_name cannot be equipped to an $player_class_name")
