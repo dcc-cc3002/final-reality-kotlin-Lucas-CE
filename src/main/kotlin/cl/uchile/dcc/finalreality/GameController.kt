aea4f86c878780a30fd07b52181a9c890376993b
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-11-25 22:17:36 -0300

    Creation GameController class with example

---
+++ src/main/kotlin/cl/uchile/dcc/finalreality/GameController.kt
@@ -0,0 +1,49 @@
+package cl.uchile.dcc.finalreality
+
+import cl.uchile.dcc.finalreality.model.character.Enemy
+import cl.uchile.dcc.finalreality.model.character.GameCharacter
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
+import cl.uchile.dcc.finalreality.model.weapon.GameWeapon
+import java.util.concurrent.LinkedBlockingQueue
+
+class GameController {
+    private val turnsQueue = LinkedBlockingQueue<GameCharacter>()
+    private val playerCharacters = mutableListOf<PlayerCharacter>()
+    private val enemyCharacters = mutableListOf<Enemy>()
+
+    init {
+        for (i in 1..10) {
+            // TODO: Add enemies to the game
+            // TODO: Add players to the game
+            // TODO: Add characters to the turns queue
+        }
+    }
+
+    fun createPlayer(name: String, hp: Int, defense: Int, weapon: GameWeapon) {
+        // TODO: Create a player character
+    }
+
+    fun createEnemy(name: String, hp: Int, defense: Int, weight: Int) {
+        // TODO: Create an enemy character
+    }
+
+    fun attack(attacker: GameCharacter, target: GameCharacter) {
+        // TODO: Attack a target
+    }
+
+    fun useMagic(attacker: GameCharacter, target: GameCharacter) {
+        // TODO: Use magic on a target
+    }
+
+    fun waitTurn(character: GameCharacter) {
+        // TODO: Call the waitTurn method of the character
+    }
+
+    fun onPlayerWin() {
+        // TODO: Handle the player winning the game
+    }
+
+    fun onEnemyWin() {
+        // TODO: Handle the enemy winning the game
+    }
+}
