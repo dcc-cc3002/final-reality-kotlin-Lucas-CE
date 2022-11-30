5b8dac4cd8cbfd90feed638e69568cdbba4f5ec1
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-11-30 14:40:24 -0300

    Fixed file name

--- src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/effects/CompositeEffect.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/model/character/player/effects/CompositeEffect.kt
@@ -1,10 +1,9 @@
 package cl.uchile.dcc.finalreality.model.character.player.effects
 
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
-import cl.uchile.dcc.finalreality.model.character.player.mages.Mages
 
 class CompositeEffect(val effects: List<Effect>) : Effect {
-    override fun applyEffect(mages: Mages, target: GameCharacter) {
-        effects.forEach { it.applyEffect(mages, target) }
+    override fun applyEffect(target: GameCharacter) {
+        effects.forEach { it.applyEffect(target) }
     }
 }
