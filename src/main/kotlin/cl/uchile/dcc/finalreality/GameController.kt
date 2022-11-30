64fc180855d66025246333b3f84b40d35f49e717
Lucas-CE <lucas.carrascoestay@gmail.com>
2022-11-30 14:54:44 -0300

    Implemented createCharacter functions in Controller

--- src/main/kotlin/cl/uchile/dcc/finalreality/GameController.kt
+++ src/main/kotlin/cl/uchile/dcc/finalreality/GameController.kt
@@ -3,6 +3,12 @@ package cl.uchile.dcc.finalreality
 import cl.uchile.dcc.finalreality.model.character.Enemy
 import cl.uchile.dcc.finalreality.model.character.GameCharacter
 import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter
+import cl.uchile.dcc.finalreality.model.character.player.common.Engineer
+import cl.uchile.dcc.finalreality.model.character.player.common.Knight
+import cl.uchile.dcc.finalreality.model.character.player.common.Thief
+import cl.uchile.dcc.finalreality.model.character.player.mages.BlackMage
+import cl.uchile.dcc.finalreality.model.character.player.mages.WhiteMage
+import cl.uchile.dcc.finalreality.model.character.player.spells.Spell
 import cl.uchile.dcc.finalreality.model.weapon.GameWeapon
 import java.util.concurrent.LinkedBlockingQueue
 
@@ -14,12 +20,55 @@ class GameController {
     init {
     }
 
-    fun createPlayer(name: String, hp: Int, defense: Int, weapon: GameWeapon) {
-        // TODO: Create a player character
+    fun createPlayerCharacterEngineer(name: String, hp: Int, defense: Int, weapon: GameWeapon) {
+        val engineer = Engineer(name, hp, defense, turnsQueue)
+        engineer.equip(weapon)
+        playerCharacters.add(engineer)
+    }
+
+    fun createPlayerCharacterKnight(name: String, hp: Int, defense: Int, weapon: GameWeapon) {
+        val knight = Knight(name, hp, defense, turnsQueue)
+        knight.equip(weapon)
+        playerCharacters.add(knight)
+    }
+
+    fun createPlayerCharacterThief(name: String, hp: Int, defense: Int, weapon: GameWeapon) {
+        val thief = Thief(name, hp, defense, turnsQueue)
+        thief.equip(weapon)
+        playerCharacters.add(thief)
+    }
+
+    fun createPlayerCharacterWhiteMage(
+        name: String,
+        hp: Int,
+        mp: Int,
+        defense: Int,
+        weapon: GameWeapon,
+        spell: Spell
+    ) {
+        val whiteMage = WhiteMage(name, hp, mp, defense, turnsQueue)
+        whiteMage.equip(weapon)
+        whiteMage.equipSpell(spell)
+        playerCharacters.add(whiteMage)
+    }
+
+    fun createPlayerCharacterBlackMage(
+        name: String,
+        hp: Int,
+        mp: Int,
+        defense: Int,
+        weapon: GameWeapon,
+        spell: Spell
+    ) {
+        val blackMage = BlackMage(name, hp, mp, defense, turnsQueue)
+        blackMage.equip(weapon)
+        blackMage.equipSpell(spell)
+        playerCharacters.add(blackMage)
     }
 
     fun createEnemy(name: String, hp: Int, defense: Int, weight: Int) {
-        // TODO: Create an enemy character
+        val enemy = Enemy(name, hp, defense, weight, turnsQueue)
+        enemyCharacters.add(enemy)
     }
 
     fun attack(attacker: GameCharacter, target: GameCharacter) {
@@ -31,7 +80,7 @@ class GameController {
     }
 
     fun waitTurn(character: GameCharacter) {
-        // TODO: Call the waitTurn method of the character
+        character.waitTurn()
     }
 
     fun onPlayerWin() {
